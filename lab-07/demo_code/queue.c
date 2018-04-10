6d9157c81ee2d7600d46f10ede213f2fdc1d646d
huynhsamha <huynhsamha@gmail.com>
2018-04-10 11:35:58 +0700

    add lab07

---
+++ lab-07/demo_code/queue.c
@@ -0,0 +1,39 @@
+
+#include <stdlib.h>
+#include "queue.h"
+#include <pthread.h>
+
+/* Remember to initilize the queue before using it */
+void initialize_queue(struct pqueue_t * q) {
+	q->head = q->tail = NULL;
+	pthread_mutex_init(&q->lock, NULL);
+}
+
+/* Return non-zero if the queue is empty */
+int empty(struct pqueue_t * q) {
+	return (q->head == NULL);
+}
+
+/* Get PCB of a process from the queue (q).
+ * Return NULL if the queue is empty */
+struct pcb_t * de_queue(struct pqueue_t * q) {
+	struct pcb_t * proc = NULL;
+	// TODO: return q->head->data and remember to update the queue's head
+	// and tail if necessary. Remember to use 'lock' to avoid race
+	// condition
+	
+	// YOUR CODE HERE
+
+	return proc;
+}
+
+/* Put PCB of a process to the queue. */
+void en_queue(struct pqueue_t * q, struct pcb_t * proc) {
+	// TODO: Update q->tail.
+	// Remember to use 'lock' to avoid race condition
+	
+	// YOUR CODE HERE
+	
+}
+
+
