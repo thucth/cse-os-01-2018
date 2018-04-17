7c20204bb144b8e81aab960d0b748680db2331fb
huynhsamha <huynhsamha@gmail.com>
2018-04-17 12:01:06 +0700

    implement queue assignment 2

--- Assignment2/source_code/src/queue.c
+++ Assignment2/source_code/src/queue.c
@@ -7,13 +7,27 @@ int empty(struct queue_t * q) {
 }
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
-	/* TODO: put a new process to queue [q] */	
+	/* TODO: put a new process to queue [q] */
+	q->proc[q->size++] = proc;
+	// End code
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	if (q->size == 0) return NULL;
+
+	int i = 0, j;
+	for (j = 1; j < q->size; j++) {
+		if (q->proc[j]->priority > q->proc[i]->priority) {
+			i = j;
+		}
+	}
+
+	struct pcb_t * res = q->proc[i];
+	q->proc[i] = q->proc[--q->size];
+	return res;
+	// End code
 }
 
